205fd79b83a301776d9f2ade0f5dc7acba5c607e
adish13 <adirocks0013@gmail.com>
2021-10-23 15:59:12 +0530

    removed venv

--- float_moodle/urls.py
+++ float_moodle/urls.py
@@ -10,6 +10,7 @@ from django.contrib.auth import views as auth_views #import this
 ## @brief url patterns for the website.
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url('',views.default,name='default'),
     # path('', include('django.contrib.auth.urls')),  
     path("password_reset", views.password_reset_request, name="password_reset"),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
