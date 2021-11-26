06457af2e60c4a48aebda4ee83f1775d98ca5bf4
adish13 <adirocks0013@gmail.com>
2021-11-26 12:19:46 +0530

    added join course feature

---
+++ instructor/functions.py
@@ -0,0 +1,15 @@
+import django.core.mail as dcm
+
+def send_email( subject, message, email_from, recipient_list, html_message ):
+    try:
+        if html_message:
+            dcm.send_mail( subject, message, email_from, recipient_list, html_message=html_message ) 
+        else :
+            dcm.send_mail( subject, message, email_from, recipient_list ) 
+        print('success', recipient_list)
+    except :
+        print('Email failed')
+
+def course_invite_text(course_name,access_code):
+    text = "Welcome to Float Moodle."+"\n" +"The access code for "+course_name + " is "+ access_code+"\n"+"You can join the course by entering this code." "\n \n Sent by team BruteForces."
+    return text
